4d3735c600b0eec87501aab7581fdec4fe5455b9
Leonardo Miyagi <lmiyagi@jera.com.br>
2017-07-13 00:21:51 -0400

    databinding example

--- app/src/main/java/br/com/leonardomiyagi/baseapplication/presentation/base/BaseActivity.java
+++ app/src/main/java/br/com/leonardomiyagi/baseapplication/presentation/base/BaseActivity.java
@@ -1,6 +1,10 @@
 package br.com.leonardomiyagi.baseapplication.presentation.base;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import dagger.android.AndroidInjection;
 
 /**
  * Created by SES\leonardom on 11/07/17.
@@ -8,4 +12,9 @@ import android.support.v7.app.AppCompatActivity;
 
 public class BaseActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        AndroidInjection.inject(this);
+        super.onCreate(savedInstanceState);
+    }
 }
--- app/src/main/java/br/com/leonardomiyagi/baseapplication/presentation/main/MainActivity.java
+++ app/src/main/java/br/com/leonardomiyagi/baseapplication/presentation/main/MainActivity.java
@@ -1,10 +1,12 @@
 package br.com.leonardomiyagi.baseapplication.presentation.main;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 
 import javax.inject.Inject;
 
 import br.com.leonardomiyagi.baseapplication.R;
+import br.com.leonardomiyagi.baseapplication.databinding.ActivityMainBinding;
 import br.com.leonardomiyagi.baseapplication.presentation.base.BaseActivity;
 import dagger.android.AndroidInjection;
 
@@ -13,10 +15,12 @@ public class MainActivity extends BaseActivity implements MainContract.View {
     @Inject
     MainContract.Presenter presenter;
 
+    private ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        AndroidInjection.inject(this);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setPresenter(presenter);
     }
 }
