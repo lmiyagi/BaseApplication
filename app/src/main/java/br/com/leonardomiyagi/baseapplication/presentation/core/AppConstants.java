72ffde095952282fb470480a551565dd9f4f3de0
Leonardo Miyagi <leonardomiyagi@gmail.com>
2018-11-14 13:53:54 -0300

    removing unused constants

--- app/src/main/java/br/com/leonardomiyagi/baseapplication/presentation/core/AppConstants.java
+++ app/src/main/java/br/com/leonardomiyagi/baseapplication/presentation/core/AppConstants.java
@@ -7,10 +7,8 @@ public class AppConstants {
     public static final String DEBUG = "debug";
     public static final String STAGING = "staging";
 
-    public static final String API_BASE_STAGING_URL = "https://www.myapiurl.com/";
     public static final String API_BASE_URL = "https://www.myapiurl.com/";
-
-    public static final String LOGIN_ACTION = "LOGIN_ACTION";
+    public static final String API_BASE_STAGING_URL = "https://www.myapiurl.com/";
 
     public static final String D_API_BASE_URL = "D_API_BASE_URL";
 }
